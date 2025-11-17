bf95a823e4035d2a6d8d6f9a2cde7ee784343f07
AGANETHED <astcvetkov1@chsu.ru>
2025-11-17 18:23:41 +0300

    feat: provider annotation

--- src/main/java/ru/chsu/exception/ValidationExceptionMapper.java
+++ src/main/java/ru/chsu/exception/ValidationExceptionMapper.java
@@ -3,9 +3,11 @@ package ru.chsu.exception;
 import jakarta.validation.ValidationException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
 import java.util.Map;
 
+@Provider
 public class ValidationExceptionMapper implements ExceptionMapper<ValidationException> {
     @Override
     public Response toResponse(ValidationException e) {
